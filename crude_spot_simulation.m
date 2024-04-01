0339493f47861aa66ea2dbb47af7c91636cd1581
vlad.burian <vlad.burian9@gmail.com>
2024-04-01 12:33:29 +0300

    added pricing simulations and options pricing

---
+++ crude_spot_simulation.m
@@ -0,0 +1,22 @@
+clear;
+clearvars;
+
+path = "C:\Users\Vlad\Desktop\Simulation\Case Study\data\DCOILBRENTEU.csv";
+data = readtable(path);
+
+data.Properties.VariableNames = ["date","crude"];
+
+returns = pct_change(data.crude); % calculate returns from spot prices
+returns = returns(~isnan(returns)); % delete NaN values
+
+x_values = linspace(-0.2, 0.2, 500);
+dist = fitdist(returns, 'kernel');
+figure;
+histogram(returns, 'Normalization','pdf');
+hold on;
+plot(x_values, pdf(dist, x_values))
+hold off;
+
+function p = pct_change(x)
+    p = diff(x)./x(1:end-1); 
+end
